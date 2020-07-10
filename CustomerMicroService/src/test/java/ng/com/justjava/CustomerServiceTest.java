5164d8b3c986949de40b5d70fd2dc6f74dde8642
Akinrinde Kazeem <akinrinde@justjava.com.ng>
2020-07-10 11:34:50 +0100

    Initial Source of Quarkus

---
+++ CustomerMicroService/src/test/java/ng/com/justjava/CustomerServiceTest.java
@@ -0,0 +1,21 @@
+package ng.com.justjava;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class CustomerServiceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/customers")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
---
+++ CustomerMicroService/src/test/java/ng/com/justjava/NativeCustomerServiceIT.java
@@ -0,0 +1,9 @@
+package ng.com.justjava;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCustomerServiceIT extends CustomerServiceTest {
+
+    // Execute the same tests but in native mode.
+}
